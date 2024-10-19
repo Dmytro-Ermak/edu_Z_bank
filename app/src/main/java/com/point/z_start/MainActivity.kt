dc8c872a021dea0f575082dde6c104c1b88b36e8
Dmytro-Ermak <snow71yul5@gmail.com>
2024-10-19 21:44:43 +0300

    1

--- app/src/main/java/com/point/z_start/MainActivity.kt
+++ app/src/main/java/com/point/z_start/MainActivity.kt
@@ -1,20 +1,11 @@
 package com.point.z_start
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+fun main() {
+    val zooShop = ZooShop()
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+    val dog = zooShop.determineAnimal("Husky", 30.0, 5)
+    dog?.who()
+
+    val cat = zooShop.determineAnimal("Siamese", 4.5, 3)
+    cat?.who()
 }
